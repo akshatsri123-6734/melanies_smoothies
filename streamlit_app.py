d8b0c9c689e291a559e1883a3b48698ac9b3686c
akshatsri123-6734 <Akshat.srivastava@Geminisolutions.com>
2026-04-23 16:15:03 +0530

    Fix data retrieval for fruit options in Streamlit app
    Updated the query to fetch fruit options from the correct table and converted the result to a pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,11 @@ st.write("The name of your smoothie will be ", name_on_order)
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
+st.dataframe(my_dataframe.to_pandas(), use_container_width=True)
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list=st.multiselect(
