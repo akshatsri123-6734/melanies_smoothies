f9c7e047828b72299f12fcff58bb02b8dabfd142
akshatsri123-6734 <Akshat.srivastava@Geminisolutions.com>
2026-04-23 15:49:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ import requests
 smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
-st.text(smoothiefroot_response.json()
+st.text(smoothiefroot_response.json())
